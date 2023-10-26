80affa5e0daf6994dd1e89b190719b048e3680ab
roddbrisson <128113468+roddbrisson@users.noreply.github.com>
2023-10-25 20:38:30 -0700

    Hour 11 (2)

--- rbrisson_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ rbrisson_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -5,28 +5,21 @@ using UnityEngine;
 public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
+    public Vector3 spawnValues;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 1; i <= 10; i++)
+        {
+            Vector3 spawnposition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 1f, Random.Range(-spawnValues.z, spawnValues.z));
+            GameObject gameObject = Instantiate<GameObject>(prefab, spawnposition, Quaternion.identity);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Whenever we hit the B key we will generate a prefab at the
-        // position of the original prefab
-        // Whenever we hit the space key, we will generate a prefab at the
-        //position of the spawn object that this script is attached to
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            Instantiate(prefab);
-        }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Instantiate(prefab, transform.position, transform.rotation);
-        }
     }
 }
