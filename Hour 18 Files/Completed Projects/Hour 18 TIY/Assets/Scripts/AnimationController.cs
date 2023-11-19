cd3e0f28e809a66dc906af0459b20be2e62ad920
roddbrisson <128113468+roddbrisson@users.noreply.github.com>
2023-11-18 22:53:57 -0800

    Hour 20

---
+++ Hour 18 Files/Completed Projects/Hour 18 TIY/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+	Animator anim;
+	void Start()
+	{
+		// Get a reference to the animator
+		anim = GetComponent<Animator>();
+	}
+	void Update()
+	{
+		anim.SetFloat("Speed", Input.GetAxis("Vertical"));
+		anim.SetFloat("Direction", Input.GetAxis("Horizontal"));
+	}
+
+}
